f78cc889c1306226228ad1966123fa3fee727078
MiriamEvardi <miriamevardi97@gmail.com>
2024-04-09 12:07:04 +0200

    base exercise completed (without stylization)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
+<div class="container">
 
+    <h1>MOVIE LIST</h1>
+    <ul class="d-flex justify-content-between list-unstyled">
+
+        @foreach($movies as $movie)
+        <li>
+            {{$movie->title}}
+            <p>
+                {{$movie->original_title}}
+            </p>
+            <p>
+                {{$movie->nationality}}
+            </p>
+            <p>
+                {{$movie->date}}
+            </p>
+            <p>
+                {{$movie->vote}}
+            </p>
+        </li>
+        @endforeach
+
+    </ul>
+</div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel-Model-Controller</title>
 
     @vite('resources/js/app.js')
 </head>
 
 <body>
-
+    @yield('content')
 
 </body>
 
